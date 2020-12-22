0ecccda2795757d2536387e51d0c39271e589d42
brian <brianhwang29@gmail.com>
2020-12-22 10:13:33 +0900

    1222 bbs view and setup

---
+++ src/main/java/com/bh/bbs/controller/homeController.java
@@ -0,0 +1,34 @@
+package com.bh.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("mainPage")
+    public String mainPage(){
+        return "home";
+    }
+
+    @GetMapping("bbsList.bo")
+    public String bbsList(){
+        return "bbsListView";
+    }
+
+    @GetMapping("login.me")
+    public String login(){
+        return "loginView";
+    }
+
+    @GetMapping("enrollForm.me")
+    public String enrollForm(){
+        return "enrollFormView";
+    }
+}
---
+++ src/main/java/com/bh/bbs/domain/Member.java
@@ -0,0 +1,16 @@
+package com.bh.bbs.domain;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Member {
+
+    private int memNo;
+    private String memName;
+    private String email;
+
+}
